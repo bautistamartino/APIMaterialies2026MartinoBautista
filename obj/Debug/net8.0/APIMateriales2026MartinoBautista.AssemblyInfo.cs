f258f881d01200cd15a4121fb71169f3fed119b2
bautistamartino <martinobautista102@gmail.com>
2026-07-01 18:24:11 -0300

    faltan las funciones

--- obj/Debug/net8.0/APIMateriales2026MartinoBautista.AssemblyInfo.cs
+++ obj/Debug/net8.0/APIMateriales2026MartinoBautista.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIMateriales2026MartinoBautista")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1a119b1c56b9419223279a69699532392920cbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIMateriales2026MartinoBautista")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIMateriales2026MartinoBautista")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
